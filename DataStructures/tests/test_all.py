0e73f1cbab49b221e4d56fbab09fd1caef7a6864
nadir.izr <nadir.izr@fdf12db7-3aba-a246-c334-511d79db4154>
2010-12-26 15:42:24 +0000

    Added parameter passing of test package to test_all.py

--- DataStructures/tests/test_all.py
+++ DataStructures/tests/test_all.py
@@ -1,15 +1,36 @@
-#!/bin/python
+#!/usr/bin/python
 
 import sys
 import os
 
-USER_PROGRAM = "yaniv"
-TEST_DIR = "wet1"
+TEST_PACKAGES = {
+  "wet1": "yaniv",
+  "wet2": "servers",
+}
+DEFAULT_PACKAGE = "wet1"
 
+# Select test package and handle command line arguments.
+args = sys.argv[1:]
+chosen_package = None
+for a in sys.argv[1:]:
+  if a in TEST_PACKAGES.keys():
+    if chosen_package is not None and chosen_package != a:
+      print "ERROR: Too many test packages specified! ('%s' AND '%s')" % (
+          chosen_package, a)
+      sys.exit(1)
+    chosen_package = a
+    args.remove(a)
+if chosen_package is None:
+  chosen_package = DEFAULT_PACKAGE
+TEST_DIR = chosen_package
+USER_PROGRAM = TEST_PACKAGES[chosen_package]
+
+# Copy over executable.
 if os.system("cp -f ../%s ." % (USER_PROGRAM)) != 0:
   print """
 ERROR: Couldn't find your program!
 It should be named '%s' and in your main program dir (one above tests)""" % (
     USER_PROGRAM)
 
-os.system("cd %s && python test_all.py %s" % (TEST_DIR, " ".join(sys.argv[1:])))
+# Run the tests.
+os.system("cd %s && python test_all.py %s" % (TEST_DIR, " ".join(args)))
