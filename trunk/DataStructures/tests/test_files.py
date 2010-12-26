3512ccf65127ae53b811f7c7ce7832a28604cadd
nadir.izr <nadir.izr@fdf12db7-3aba-a246-c334-511d79db4154>
2010-12-26 12:42:14 +0000

    Added copying of executable prior to run

--- trunk/DataStructures/tests/test_files.py
+++ trunk/DataStructures/tests/test_files.py
@@ -2,4 +2,14 @@
 
 import sys
 import os
-os.system("python wet1/test_all.py %s" % " ".join(sys.argv[1:]))
+
+USER_PROGRAM = "yaniv"
+TEST_DIR = "wet1"
+
+if os.system("cp ../%s ." % (USER_PROGRAM)) != 0:
+  print """
+ERROR: Couldn't find your program!
+It should be named '%s' and in your main program dir (one above tests)""" % (
+    USER_PROGRAM)
+
+os.system("python %s/test_all.py %s" % (TEST_DIR, " ".join(sys.argv[1:])))
