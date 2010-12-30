2efb6d2c45a543a80e6d78e0b68e1e9884fdfd21
igal1987 <igal1987@fdf12db7-3aba-a246-c334-511d79db4154>
2010-12-30 09:57:23 +0000

    added the test

---
+++ DataStructures/tests/wet2/AVLTreeUpdateTest.cpp
@@ -0,0 +1,310 @@
+#include <stdlib.h>
+
+#include "test.h"
+#include "util.h"
+#include "../AVLTree.h"
+
+const int items_in_random_test = 100;
+const int random_tests = 500;
+
+class IdWithDelta {
+public:
+  int id;
+  int delta;
+  IdWithDelta(int id_, int delta_) : id(id_), delta(delta_) {}
+  IdWithDelta(const IdWithDelta& o) : id(o.id), delta(o.delta) {}
+  bool operator<(const IdWithDelta& o) { return id < o.id; }
+};
+
+/*
+ * Performs a general rotation test, in which it inserts the given number into
+ * a new tree, and compares an in-order walk over the tree to the sorted
+ * numbers and their respective expected heights in the result tree.
+ * @param numbers the array of numbers to insert (size=n)
+ * @param sorted the array of sorted numbers to compare to (size=n)
+ * @param heights the array of heights to compare result to (size=n)
+ * @param deltas the array of expected deltas to compare to (size=n)
+ * @param n the size of the arrays
+ * @return true if the test succeeded, false otherwise
+ */
+bool testRotation(const AVLTree<IdWithDelta>& tree,
+                  int *sorted,  int *heights, int *deltas, int n) {
+  // go over the tree and check the heights
+  AVLTree<IdWithDelta>::Iterator iter = tree.begin();
+  for (int i = 0; i < n; ++i) {
+    ASSERT_TRUE(iter != tree.end());
+    ASSERT_EQUALS(sorted[i], iter.value().id);
+    ASSERT_EQUALS(heights[i], iter.height());
+    ASSERT_EQUALS(deltas[i], iter.value().delta);
+    ++iter;
+  }
+  // check that there are no more elements in the tree
+  ASSERT_TRUE(iter == tree.end());
+  
+  return true;
+}
+
+/*
+ * Verify that the tree looks like:
+ *     4
+ *    / \
+ *   2   5
+ *  / \
+ * 1   3
+ *
+ * By forcing 2 LL rotations to occur.
+ */
+bool testInsertRotationLL() {
+  AVLTree<IdWithDelta> tree;
+  ASSERT_TRUE(tree.insert(IdWithDelta(5, 5)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(4, 4)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(3, 3)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(2, 2)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(1, 1)));
+
+  int sorted[]  = { 1, 2, 3,4,5};
+  int heights[] = { 0, 1, 0,2,0};
+  int deltas[]  = {-1,-2,-1,4,5}; 
+  return testRotation(tree, sorted, heights, deltas, 5);
+}
+
+/*
+ * Verify that the tree looks like:
+ *
+ *     4
+ *    / \
+ *   2   5
+ *  / \
+ * 1   3
+ *
+ * By forcing 2 LR rotations to occur.
+ */
+bool testInsertRotationLR() {
+  AVLTree<IdWithDelta> tree;
+  ASSERT_TRUE(tree.insert(IdWithDelta(5, 5)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(3, 3)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(4, 4)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(1, 1)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(2, 2)));
+
+  int sorted[]  = { 1 ,2, 3,4,5};
+  int heights[] = { 0 ,1, 0,2,0};
+  int deltas[]  = {-1,-2,-1,4,5}; 
+  return testRotation(tree, sorted, heights, deltas, 5);
+}
+
+/*
+ * Verify that the tree looks like:
+ *
+ *      2
+ *     / \
+ *    1   4
+ *       / \
+ *      3   5
+ *
+ * By forcing 2 RR rotations to occur.
+ */
+bool testInsertRotationRR() {
+  AVLTree<IdWithDelta> tree;
+  ASSERT_TRUE(tree.insert(IdWithDelta(1, 1)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(2, 2)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(3, 3)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(4, 4)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(5, 5)));
+
+  int sorted[]  = { 1,2, 3,4,5};
+  int heights[] = { 0,2, 0,1,0};
+  int deltas[]  = {-1,2,-1,4,5};
+  return testRotation(tree, sorted, heights, deltas, 5);
+}
+
+/*
+ * Verify that the tree looks like:
+ *
+ *      2
+ *     / \
+ *    1   4
+ *       / \
+ *      3   5
+ *
+ * By forcing 2 RL rotations to occur.
+ */
+bool testInsertRotationRL() {
+  AVLTree<IdWithDelta> tree;
+  ASSERT_TRUE(tree.insert(IdWithDelta(1, 1)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(3, 3)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(2, 2)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(5, 5)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(4, 4)));
+
+  int sorted[]  = { 1,2, 3,4,5};
+  int heights[] = { 0,2, 0,1,0};
+  int deltas[]  = {-1,2,-1,4,5};
+  return testRotation(tree, sorted, heights, deltas, 5);
+}
+
+/*
+ * Verify that the tree looks like:
+ * insert 5,4,3,2,1:
+ *     4
+ *    / \
+ *   2   5
+ *  / \
+ * 1   3
+ *
+ * remove 5:
+ *     4        2
+ *    /   LL   / \
+ *   2    =>  1   4
+ *  / \          /
+ * 1   3        3
+ *
+ * remove 1:
+ *  2          3
+ *   \   RL   / \
+ *    4  =>  2   4
+ *   /
+ *  3
+ *
+ * remove 4, and insert 5,4,6:
+ *   3
+ *  / \
+ * 2   5
+ *    / \
+ *   4   6
+ *
+ * remove 2:
+ *   3           5
+ *    \    RR   / \
+ *     5   =>  3   6
+ *    / \       \
+ *   4   6       4
+ *
+ * remove 6:
+ *   5        4
+ *  /   LR   / \
+ * 3    =>  3   5
+ *  \
+ *   4
+ */
+bool testRemoveRotations() {
+  AVLTree<IdWithDelta> tree;
+  ASSERT_TRUE(tree.insert(IdWithDelta(5, 5)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(4, 4)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(3, 3)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(2, 2)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(1, 1)));
+  int sorted[]  = { 1, 2, 3,4,5};
+  int heights[] = { 0, 1, 0,2,0};
+  int deltas[]  = {-1,-2,-1,4,5};
+  ASSERT_TRUE(testRotation(tree, sorted, heights, deltas, 5));
+  
+  ASSERT_TRUE(tree.remove(IdWithDelta(5, 0)));
+  int sorted2[]  = { 1, 2,3,4};
+  int heights2[] = { 0, 2,0,1};
+  int deltas2[]  = {-1,-1,3,4};
+  ASSERT_TRUE(testRotation(tree, sorted2, heights2, deltas2, 4));
+  
+  ASSERT_TRUE(tree.remove(IdWithDelta(1, 0)));
+  int sorted3[]  = { 2,3,4};
+  int heights3[] = { 0,1,0};
+  int deltas3[]  = {-1,3,4};
+  ASSERT_TRUE(testRotation(tree, sorted3, heights3, deltas3, 3));
+  
+  ASSERT_TRUE(tree.remove(IdWithDelta(4, 0)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(5, 5)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(4, 4)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(6, 6)));
+  int sorted4[]  = { 2,3, 4,5,6};
+  int heights4[] = { 0,2, 0,1,0};
+  int deltas4[]  = {-1,3,-1,5,6};
+  ASSERT_TRUE(testRotation(tree, sorted4, heights4, deltas4, 5));
+
+  ASSERT_TRUE(tree.remove(IdWithDelta(2, 0)));
+  int sorted5[]  = { 3,4,5,6};
+  int heights5[] = { 1,0,2,0};
+  int deltas5[]  = {-1,4,5,6};
+  ASSERT_TRUE(testRotation(tree, sorted5, heights5, deltas5, 4));
+
+  ASSERT_TRUE(tree.remove(IdWithDelta(6, 0)));
+  int sorted6[]  = { 3,4,5};
+  int heights6[] = { 0,1,0};
+  int deltas6[]  = {-1,4,5};
+  ASSERT_TRUE(testRotation(tree, sorted6, heights6, deltas6, 3));
+
+  return true;
+}
+
+/*
+ * Taken from an example during the lecture.
+ * After initial insertion of 5,2,8,1,4,6,10,3,7,9,12,11 the Tree should look like that:
+ *     ___5___
+ *    /       \
+ *   2       __8__
+ *  / \     /     \
+ * 1   4   6      10
+ *    /      \    /  \
+ *   3        7  9   12
+ *                   /
+ *                  11
+ *
+ * After removing the 1, we should get a double rotation that will make
+ * the tree look like:
+ * First rotation RL:
+ *     ___5___
+ *    /       \
+ *   3       __8__
+ *  / \     /     \
+ * 2   4   6      10
+ *          \    /  \
+ *           7  9   12
+ *                  /
+ *                 11
+ * and the second rotation, RR:
+ *       ___8___
+ *      /       \
+ *     5        10
+ *    / \      /  \
+ *   3   6    9   12
+ *  / \   \       /
+ * 2   4   7     11
+ */
+bool testRemoveWithDoubleRotation() {
+  AVLTree<IdWithDelta> tree;
+  ASSERT_TRUE(tree.insert(IdWithDelta(5, 5)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(2, 2)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(8, 8)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(1, 1)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(4, 4)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(6, 6)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(10, 10)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(3, 3)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(7, 7)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(9, 9)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(12, 12)));
+  ASSERT_TRUE(tree.insert(IdWithDelta(11, 11)));
+  int sorted[]  = { 1, 2, 3, 4,5, 6, 7,8, 9,10,11,12};
+  int heights[] = { 0, 2, 0, 1,4, 1, 0,3, 0, 2, 0, 1};
+  int deltas[]  = {-1,-3,-1,-1,5,-2,-1,8,-1,10,-1,12};
+  ASSERT_TRUE(testRotation(tree, sorted, heights, deltas, 12));
+
+  ASSERT_TRUE(tree.remove(IdWithDelta(1, 0)));
+  int sorted2[]  = { 2, 3, 4, 5, 6, 7,8, 9,10,11,12};
+  int heights2[] = { 0, 1, 0, 2, 1, 0,3, 0, 2, 0, 1};
+  int deltas2[]  = {-1,-2,-1,-3,-2,-1,8,-1,10,-1,12};
+  ASSERT_TRUE(testRotation(tree, sorted2, heights2, deltas2, 11));
+
+  return true;
+}
+
+int main(int argc, char **argv) {
+	// initialize random number generator
+	srand( time(NULL) );
+
+  RUN_TEST(testInsertRotationLL);
+  RUN_TEST(testInsertRotationLR);
+  RUN_TEST(testInsertRotationRR);
+  RUN_TEST(testInsertRotationRL);
+  RUN_TEST(testRemoveRotations);
+  RUN_TEST(testRemoveWithDoubleRotation);
+}
