60d679ff33e38bb5316fc4766d731a9856706b4e
nadir.izr <nadir.izr@fdf12db7-3aba-a246-c334-511d79db4154>
2010-12-30 13:52:45 +0000

    Added an updater to the test. Fixed the rotation tests. Still has problem in removal tests.

--- trunk/DataStructures/tests/wet2/AVLTreeUpdateTest.cpp
+++ trunk/DataStructures/tests/wet2/AVLTreeUpdateTest.cpp
@@ -10,8 +10,48 @@ const int random_tests = 500;
 struct IdWithDelta {
   int id;
   int delta;
+
   IdWithDelta(int id_, int delta_) : id(id_), delta(delta_) {}
   bool operator<(const IdWithDelta& o) const { return id < o.id; }
+};
+
+class IdWithDeltaUpdater : public NodeUpdater<IdWithDelta> {
+public:
+  IdWithDeltaUpdater(AVLTree<IdWithDelta>* tree_) : tree(tree_) {}
+
+  virtual void updateOnLeftRotation(
+      IdWithDelta* value_father, IdWithDelta* value_left,
+      IdWithDelta* value_right) {
+    value_father->delta += value_right->delta;
+  }
+
+  virtual void updateOnRightRotation(
+      IdWithDelta* value_father, IdWithDelta* value_left,
+      IdWithDelta* value_right) {
+    value_left->delta -= value_father->delta;
+  }
+
+  virtual void updateOnSubstitute(
+      IdWithDelta* to_remove, IdWithDelta* to_substitute) {
+    // If this was a leaf, do nothing.
+    if (to_substitute == NULL) {
+      return;
+    }
+
+    // If this was a parent with a single left child, add the parent's delta to
+    // the child delta.
+    if ((*to_substitute) < (*to_remove)) {
+      to_substitute->delta += to_remove->delta;
+      return;
+    }
+
+    // If the substitute is bigger than the node to remove, then just replace
+    // the delta.
+    to_substitute->delta = to_remove->delta;
+  }
+
+private:
+  AVLTree<IdWithDelta>* tree;
 };
 
 /*
@@ -31,9 +71,9 @@ bool testUpdateRotation(const AVLTree<IdWithDelta>& tree,
   AVLTree<IdWithDelta>::Iterator iter = tree.begin();
   for (int i = 0; i < n; ++i) {
     ASSERT_TRUE(iter != tree.end());
-    ASSERT_EQUALS(sorted[i], iter.value().id);
+    ASSERT_EQUALS(sorted[i], iter->id);
     ASSERT_EQUALS(heights[i], iter.height());
-    ASSERT_EQUALS(deltas[i], iter.value().delta);
+    ASSERT_EQUALS(deltas[i], iter->delta);
     ++iter;
   }
   // check that there are no more elements in the tree
@@ -54,6 +94,8 @@ bool testUpdateRotation(const AVLTree<IdWithDelta>& tree,
  */
 bool testUpdateInsertRotationLL() {
   AVLTree<IdWithDelta> tree;
+  IdWithDeltaUpdater updater(&tree);
+  tree.setUpdater(&updater);
   ASSERT_TRUE(tree.insert(IdWithDelta(5, 5)));
   ASSERT_TRUE(tree.insert(IdWithDelta(4, 4)));
   ASSERT_TRUE(tree.insert(IdWithDelta(3, 3)));
@@ -62,7 +104,7 @@ bool testUpdateInsertRotationLL() {
 
   int sorted[]  = { 1, 2, 3,4,5};
   int heights[] = { 0, 1, 0,2,0};
-  int deltas[]  = {-1,-2,-1,4,5}; 
+  int deltas[]  = { 1, 5, 3,9,5}; 
   return testUpdateRotation(tree, sorted, heights, deltas, 5);
 }
 
@@ -79,15 +121,17 @@ bool testUpdateInsertRotationLL() {
  */
 bool testUpdateInsertRotationLR() {
   AVLTree<IdWithDelta> tree;
+  IdWithDeltaUpdater updater(&tree);
+  tree.setUpdater(&updater);
   ASSERT_TRUE(tree.insert(IdWithDelta(5, 5)));
   ASSERT_TRUE(tree.insert(IdWithDelta(3, 3)));
   ASSERT_TRUE(tree.insert(IdWithDelta(4, 4)));
   ASSERT_TRUE(tree.insert(IdWithDelta(1, 1)));
   ASSERT_TRUE(tree.insert(IdWithDelta(2, 2)));
 
-  int sorted[]  = { 1 ,2, 3,4,5};
-  int heights[] = { 0 ,1, 0,2,0};
-  int deltas[]  = {-1,-2,-1,4,5}; 
+  int sorted[]  = { 1,2, 3,4,5};
+  int heights[] = { 0,1, 0,2,0};
+  int deltas[]  = {-1,1,-1,9,5}; 
   return testUpdateRotation(tree, sorted, heights, deltas, 5);
 }
 
@@ -104,6 +148,8 @@ bool testUpdateInsertRotationLR() {
  */
 bool testUpdateInsertRotationRR() {
   AVLTree<IdWithDelta> tree;
+  IdWithDeltaUpdater updater(&tree);
+  tree.setUpdater(&updater);
   ASSERT_TRUE(tree.insert(IdWithDelta(1, 1)));
   ASSERT_TRUE(tree.insert(IdWithDelta(2, 2)));
   ASSERT_TRUE(tree.insert(IdWithDelta(3, 3)));
@@ -129,6 +175,8 @@ bool testUpdateInsertRotationRR() {
  */
 bool testUpdateInsertRotationRL() {
   AVLTree<IdWithDelta> tree;
+  IdWithDeltaUpdater updater(&tree);
+  tree.setUpdater(&updater);
   ASSERT_TRUE(tree.insert(IdWithDelta(1, 1)));
   ASSERT_TRUE(tree.insert(IdWithDelta(3, 3)));
   ASSERT_TRUE(tree.insert(IdWithDelta(2, 2)));
@@ -137,7 +185,7 @@ bool testUpdateInsertRotationRL() {
 
   int sorted[]  = { 1,2, 3,4,5};
   int heights[] = { 0,2, 0,1,0};
-  int deltas[]  = {-1,2,-1,4,5};
+  int deltas[]  = {-4,5,-6,9,5};
   return testUpdateRotation(tree, sorted, heights, deltas, 5);
 }
 
@@ -187,6 +235,8 @@ bool testUpdateInsertRotationRL() {
  */
 bool testUpdateRemoveRotations() {
   AVLTree<IdWithDelta> tree;
+  IdWithDeltaUpdater updater(&tree);
+  tree.setUpdater(&updater);
   ASSERT_TRUE(tree.insert(IdWithDelta(5, 5)));
   ASSERT_TRUE(tree.insert(IdWithDelta(4, 4)));
   ASSERT_TRUE(tree.insert(IdWithDelta(3, 3)));
@@ -194,13 +244,13 @@ bool testUpdateRemoveRotations() {
   ASSERT_TRUE(tree.insert(IdWithDelta(1, 1)));
   int sorted[]  = { 1, 2, 3,4,5};
   int heights[] = { 0, 1, 0,2,0};
-  int deltas[]  = {-1,-2,-1,4,5};
+  int deltas[]  = { 1, 5, 3,9,5}; 
   ASSERT_TRUE(testUpdateRotation(tree, sorted, heights, deltas, 5));
   
   ASSERT_TRUE(tree.remove(IdWithDelta(5, 0)));
   int sorted2[]  = { 1, 2,3,4};
   int heights2[] = { 0, 2,0,1};
-  int deltas2[]  = {-1,-1,3,4};
+  int deltas2[]  = { 1, 5,3,9};
   ASSERT_TRUE(testUpdateRotation(tree, sorted2, heights2, deltas2, 4));
   
   ASSERT_TRUE(tree.remove(IdWithDelta(1, 0)));
@@ -269,6 +319,8 @@ bool testUpdateRemoveRotations() {
  */
 bool testUpdateRemoveWithDoubleRotation() {
   AVLTree<IdWithDelta> tree;
+  IdWithDeltaUpdater updater(&tree);
+  tree.setUpdater(&updater);
   ASSERT_TRUE(tree.insert(IdWithDelta(5, 5)));
   ASSERT_TRUE(tree.insert(IdWithDelta(2, 2)));
   ASSERT_TRUE(tree.insert(IdWithDelta(8, 8)));
