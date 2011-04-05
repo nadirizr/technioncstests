8f36cd51f1a623614d6372ada9c8015de693eec2
nadir.izr <nadir.izr@fdf12db7-3aba-a246-c334-511d79db4154>
2011-04-05 17:14:20 +0000

    Fixed up the test a bit.

--- trunk/OperSystems/tests/ex1/test_scenarios.cpp
+++ trunk/OperSystems/tests/ex1/test_scenarios.cpp
@@ -11,12 +11,21 @@ bool testMakeGoodProcessesFixSwapperTag() {
   int init_tag = gettag(1);
   int new_tag = (swapper_tag < init_tag ? init_tag : swapper_tag) + 100;
   int num_parents = 0;
+  int pid_array[10];
+  int pid_count = 0;
 
   ASSERT_TRUE(settag(getpid(), new_tag) == 0);
   num_parents = makegoodprocesses();
   ASSERT_TRUE(num_parents > 0);
   ASSERT_TRUE(gettag(0) == (new_tag + num_parents - 1));
   ASSERT_TRUE(gettag(1) == (new_tag + num_parents - 2));
+
+  pid_count = getgoodprocesses(pid_array, 10);
+  ASSERT_TRUE(pid_count > 2);
+  ASSERT_TRUE(pid_array[0] == 0);
+  ASSERT_TRUE(pid_array[1] == 1);
+
+  return true;
 }
 
 int main() {
