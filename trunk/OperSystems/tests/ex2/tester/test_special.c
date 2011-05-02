835e89f6054b5cf27117a74682569ae3ecc5c888
OdedMesser <OdedMesser@fdf12db7-3aba-a246-c334-511d79db4154>
2011-05-02 08:51:26 +0000

    Set up the first test as an actual test.

--- trunk/OperSystems/tests/ex2/tester/test_special.c
+++ trunk/OperSystems/tests/ex2/tester/test_special.c
@@ -1,25 +1,31 @@
-#include <sched.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
+#include <string>
 
+#include <sched.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <errno.h>
+
+#include "test.h"
 #include "hw2_syscalls.h"
 
-int main() {
+/*
+ * This test checks that the parent runs before it's two children, and that the
+ * second child runs before the first.
+ * It does this by setting itself as a short process, then creating two
+ * children, and setting their requested time to be higher. Since the
+ * differences are less than 10 jiffies at first, the parent should be able to
+ * run and eventually the second child should be able to run before the first.
+ */
+bool testTwoChildren_SecondChildBeforeFirstChild() {
   int child_pid1, child_pid2;
   int a = 0, b = 1, c;
   struct sched_param param = { 100 };
   sched_setscheduler(getpid(), SCHED_SHORT, &param);
 
-  printf("// clock = %d\n", time(NULL));
-  getchar();
-
   child_pid1 = fork();
   if (child_pid1 == 0) {
-    printf("// %d 6) This is the first child! (PID=%d)\n", time(NULL), getpid());
-    fflush(stdout);
-    
+    // This is the first child.
     param.sched_priority = 10000;
     sched_setscheduler(getpid(), SCHED_SHORT, &param);
     
@@ -32,8 +38,7 @@ int main() {
     // This is the parent.
     child_pid2 = fork();
     if (child_pid2 == 0) {
-      printf("// %d 3) This is the second child! (PID=%d)\n", time(NULL), getpid());
-      fflush(stdout);
+      // This is the second child.
       param.sched_priority = 9900;
       sched_setscheduler(getpid(), SCHED_SHORT, &param);
 
@@ -43,18 +48,20 @@ int main() {
         b = c;
       }
     } else {
-      printf("// %d 1) This is the parent.\n", time(NULL));
-      fflush(stdout);
-      printf("// %d 2) Waiting for first child ...\n", time(NULL));
-      fflush(stdout);
-      printf("// %d 4) %d Done.\n", time(NULL), wait());
-      fflush(stdout);
-      printf("// %d 5) Waiting for second child ...\n", time(NULL));
-      fflush(stdout);
-      printf("// %d 7) %d Done.\n", time(NULL), wait());
-      fflush(stdout);
+      // This is the parent.
+      ASSERT_TRUE(wait() == child_pid2);
+      ASSERT_TRUE(wait() == child_pid1);
     }
   }
   
+  return true;
+}
+
+int main() {
+	// initialize random number generator
+	srand( time(NULL) );
+
+  RUN_TEST(testTwoChildren_SecondChildBeforeFirstChild);
+
   return 0;
 }
