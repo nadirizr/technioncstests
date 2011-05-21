111c642a8f64dbf6d1bbe04bb38258a7a06516a5
igal1987 <igal1987@fdf12db7-3aba-a246-c334-511d79db4154>
2011-05-21 00:35:01 +0000

    added a test for hash table

--- trunk/OperSystems/tests/ex3/unittests/hash_table_test.c
+++ trunk/OperSystems/tests/ex3/unittests/hash_table_test.c
@@ -1,12 +1,38 @@
+#include <stdlib.h>
 #include "hash_table_test.h"
 #include "../../src/hash_table.h"
 
 test_result_t* test_hash_table_basic() {
   hash_table_t* ht = ht_init(100);
+  int i, *value, *keys, **values;
 
   ASSERT_NOT_NULL(ht, "Bad hash table init");
-  FAIL("Not implemented");
 
+  for (i = 0; i < 1000; ++i) {
+    value = malloc(sizeof(int));
+    (*value) = 10*i;
+    ht_put(ht, i, value);
+  }
+
+  ASSERT_EQUALS_INT(1000, ht_num_elements(ht), "Bad number of elements");
+
+  keys = malloc(1000*sizeof(int));
+  ASSERT_EQUALS_INT(1000, ht_all_keys(ht, keys), "Bad number  of keys");
+  ASSERT_EQUALS_INT(1000, ht_all_values(ht, (ht_value_t*)values), "Bad number of values");
+  for (i = 0; i < 1000; ++i) {
+    ASSERT_CONTAINS(keys, 1000, i, "Missing key");
+    ASSERT_EQUALS_INT(10*i, ((int)(*((int*)ht_get(ht, i)))), "Missing value");
+    ASSERT(ht_has_key(ht, i), "Missing key");
+  }
+
+  for (i = 0; i < 1000; ++i) {
+    ASSERT(ht_remove(ht, i), "Element was not removed");
+    ASSERT_EQUALS_INT(1000-i-1, ht_num_elements(ht), "Item not removed from counter");
+  }
+  ASSERT_EQUALS_INT(0, ht_all_keys(ht, keys), "Not all keys removed");
+
+  free(keys);
+  free(values);
   ht_destroy(ht);
   TEST_SUCCESS();
 }
