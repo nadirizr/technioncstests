b293f179f98ae8cc772baa75590b6ff6b27aca70
nadir.izr <nadir.izr@fdf12db7-3aba-a246-c334-511d79db4154>
2011-05-22 17:43:55 +0000

    Simulator seems to work.

--- OperSystems/tests/ex3/simulator/logic.py
+++ OperSystems/tests/ex3/simulator/logic.py
@@ -1,81 +1,137 @@
 from sys import stderr
 
-from tag_util import *
+ERROR_STOP = -10000
 
-ESRCH = 3
-EINVAL = 22
+class Message:
+  def __init__(self, origin, destination, message, is_sync, is_urgent, is_broadcast):
+    self.origin = origin
+    self.destination = destination
+    self.message = message
+    self.is_sync = is_sync
+    self.is_urgent = is_urgent
+    self.is_broadcast = is_broadcast
 
-class TagsLogic:
+class ThreadsLogic:
 
-  def __init__(self):
-    self.state = MainState()
-    self.tag = 0
+  def __init__(self, num_threads):
+    self.num_threads = num_threads
+    self.messages_for_thread = {}
+    for t in range(1, self.num_threads + 1):
+      self.messages_for_thread[t] = []
 
-  def createChild(self, process_indexes):
-    p = self.state.getProcessForIndexes(process_indexes)
-    new_process = self.state.addNewProcess(p.tag / 2, p)
-    return self.state.getPIDForProcess(new_process)
+    self.barrier_counter = -1
+    self.waiting_at_barrier = []
 
-  def getTag(self, process_indexes, pid):
-    if 0 <= pid < len(self.state.getProcesses()):
-      return self.state.getProcessForPID(pid).tag
-    return -ESRCH
+    self.waiting_broadcasters = {}
+    self.finished_broadcasters = []
 
-  def setTag(self, process_indexes, pid, tag):
-    if not (0 <= pid < len(self.state.getProcesses())):
-      return -ESRCH
+  def close(self, thread_index):
+    self.barrier_counter = -1
+    self.waiting_at_barrier = []
+    self.messages_for_thread = {}
+    self.num_threads = 0
+    self.waiting_broadcasters = {}
+    self.finished_broadcasters = []
 
-    if tag < 0:
-      return -EINVAL
+  def createBarrier(self, thread_index, n):
+    if n < 1:
+      return -1
 
-    p = self.state.getProcessForIndexes(process_indexes)
-    if pid == self.state.getPIDForProcess(p):
-      p.tag = tag
-      return 0
-    for c in p.children:
-      if pid == self.state.getPIDForProcess(c):
-        c.tag = tag
-        return 0
-    return -EINVAL
+    if thread_index in self.waiting_at_barrier or \
+       thread_index in self.waiting_broadcasters:
+      return ERROR_STOP
 
-  def getGoodProcesses(self, process_indexes, count):
-    if count < 0:
-      return -EINVAL
-    
-    pids = [pid for pid in self.state.getPIDs()
-            if self.__isProcessGood(self.state.getProcessForPID(pid))]
-    pids.sort()
-    
-    if self.tag > self.state.getMainProcess().tag:
-      pids = [-1] + pids
-    pids = [0, 1] + pids
-
-    return pids[:count]
-
-  def makeGoodProcesses(self, process_indexes):
-    if not self.state.getProcesses():
-      return 0
-
-    p = self.state.getProcessForIndexes(process_indexes)
-    num_checked = 1
-    while p is not None:
-      if not self.__isProcessGood(p):
-        p.tag = self.__sumChildrenTags(p) + 1
-      num_checked += 1
-      p = p.parent
-
-    if self.tag <= self.state.getMainProcess().tag:
-      self.tag = self.state.getMainProcess().tag + 1
-
-    return num_checked
-
-  def close(self, process_indexes):
-    self.state.removeProcessAndChildren(
-        self.state.getProcessForIndexes(process_indexes))
+    self.barrier_counter = n
+    self.waiting_at_barrier = []
     return 0
-    
-  def __isProcessGood(self, process):
-    return process.tag > self.__sumChildrenTags(process)
+  
+  def destroyBarrier(self, thread_index):
+    if thread_index in self.waiting_at_barrier or \
+       thread_index in self.waiting_broadcasters:
+      return ERROR_STOP
 
-  def __sumChildrenTags(self, process):
-    return reduce(lambda x,y:x+y, [c.tag for c in process.children], 0)
+    self.barrier_counter = -1
+    self.waiting_at_barrier = []
+
+  def barrier(self, thread_index):
+    if self.barrier_counter < 0:
+      return -1
+
+    if thread_index in self.waiting_at_barrier or \
+       thread_index in self.waiting_broadcasters:
+      return ERROR_STOP
+
+    self.waiting_at_barrier.append(thread_index)
+
+    if len(self.waiting_at_barrier) == self.barrier_counter:
+      passed = self.waiting_at_barrier[:]
+      self.waiting_at_barrier = []
+      self.barrier_counter = -1
+      return passed
+    return 0
+
+  def send(self, thread_index, to, is_sync, is_urgent, message):
+    if to not in self.messages_for_thread:
+      return -1
+
+    if thread_index in self.waiting_at_barrier or \
+       thread_index in self.waiting_broadcasters:
+      return ERROR_STOP
+
+    message = Message(thread_index, to, message, is_sync, is_urgent, False)
+    if is_urgent:
+      self.messages_for_thread[to].insert(0, message)
+    else:
+      self.messages_for_thread[to].append(message)
+
+    if not is_sync:
+      return 0
+    return 1
+
+  def broadcast(self, thread_index, is_sync, is_urgent, message):
+    if thread_index in self.waiting_at_barrier or \
+       thread_index in self.waiting_broadcasters:
+      return ERROR_STOP
+
+    destinations = []
+    for (t, q) in self.messages_for_thread.items():
+      if t == thread_index:
+        continue
+
+      message_t = Message(thread_index, t, message, is_sync, is_urgent, True)
+      if is_urgent:
+        q.insert(0, message_t)
+      else:
+        q.append(message_t)
+
+      destinations.append(t)
+
+    if not is_sync:
+      return 0
+
+    self.waiting_broadcasters[thread_index] = destinations
+    return 1
+
+  def deliverAllMessages(self):
+    delivered = []
+    for (t, q) in self.messages_for_thread.items():
+      if t in self.waiting_at_barrier:
+        continue
+
+      for m in q:
+        if m.is_broadcast and m.origin in self.waiting_broadcasters:
+          self.waiting_broadcasters[m.origin].remove(m.destination)
+
+          if not self.waiting_broadcasters[m.origin]:
+            del self.waiting_broadcasters[m.origin]
+            self.finished_broadcasters.append(m.origin)
+
+      delivered += q
+      self.messages_for_thread[t] = []
+
+    return delivered
+
+  def finishedBroadcasters(self):
+    finished = self.finished_broadcasters[:]
+    self.finished_broadcasters = []
+    return finished
--- OperSystems/tests/ex3/simulator/simulator.py
+++ OperSystems/tests/ex3/simulator/simulator.py
@@ -2,8 +2,6 @@
 
 # Given a file of input commands in the format described in commands.txt,
 # outputs the results of the various commands.
-# This simulator assumes that the testing process is the son of the init
-# process, as it is run using the tag_launcher.
 #
 # Called as:
 # ./simulator.py <input_file>
@@ -12,78 +10,197 @@ import sys
 
 from logic import *
 
-class TagsParser:
+OK_STOP = -9999
+ERROR_STOP = -10000
+
+THREAD_PREFIX   = "[Thread %d]: %s"
+THREAD_ERROR    = "[Thread %d]: ERROR [line %d]: %s!"
+THREAD_ERROR_RC = "[Thread %d]: ERROR [line %d]: %s (rc = %d)!"
+
+class ThreadsParser:
 
   def __init__(self):
-    self.logic = TagsLogic()
+    self.logic = None
+    self.num_threads = 0
 
   def parse(self, input_stream):
+    line_num = 1
     line = input_stream.readline()
     while line:
-      self.parseLine(line)
+      rc = self.parseLine(line, line_num)
+
+      if rc == ERROR_STOP:
+        print "ERROR: Unrecoverable Error!"
+        break
+      if rc == OK_STOP:
+        break
+
+      line_num += 1
       line = input_stream.readline()
 
-  def parseLine(self, line):
+  def parseLine(self, line, line_num):
+    # Get the arguments, and the command.
     args = line.split()
     if not args:
       print
-      return
-
+      return 0
     cmd = args[0]
+
+    # Handle empty lines and comments.
     if not cmd or cmd[0] == "#":
       print line,
       return 0
 
-    process_indexes = []
-    if ("/" in args[0]) or args[0][0].isdigit():
-      process_indexes = args[0].split("/")
-      process_indexes = [int(a) for a in process_indexes]
+    # Get the thread index, if it exists.
+    thread_index = -1
+    if args[0][0].isdigit():
+      thread_index = int(args[0])
       cmd = args[1]
+      args = args[1:]
+
+    # Handle the inialization command.
+    if not self.logic:
+      if cmd != "INIT":
+        return ERROR_STOP
+
+      self.num_threads = int(args[-1])
+      self.logic = ThreadsLogic(self.num_threads)
+
+      print "Main Thread Registered"
+      for t in range(1, self.num_threads + 1):
+        print THREAD_PREFIX % (t, "Registered (Thread ID = %d)" % t)
+      return 0
+
+    if self.logic and cmd == "INIT":
+      return ERROR_STOP
+
+    # Handle commands that don't require a thread ID.
+    if cmd == "CLOSE" or cmd == "NOP":
+      if thread_index < 0:
+        thread_index = 1
+
+    # If we don't have a thread index by now, this is an error.
+    if thread_index < 0 or thread_index > self.num_threads:
+      print "ERROR [line %d]: Invalid thread index %d!" % (
+          line_num, thread_index)
+      return ERROR_STOP
+
+    # Handle the various commands.
+    rc = 0
+
+    if cmd == "NOP":
+      rc = 0
+
+    elif cmd == "CREATE_BARRIER":
+      n = int(args[-1])
+      rc = self.logic.createBarrier(thread_index, n)
       
-    if cmd == "CREATE_CHILD":
-      rc = self.logic.createChild(process_indexes)
-      print "DONE %d" % rc
-      return rc
+      if rc < 0:
+        print THREAD_ERROR % (
+            thread_index, line_num, "Barrier Creation Failed")
+      else:
+        print THREAD_PREFIX % (thread_index, "Barrier Created")
 
-    elif cmd == "GET_TAG":
-      pid = int(args[-1])
-      rc = self.logic.getTag(process_indexes, pid)
-      print "DONE %d" % rc
-      return rc
+    elif cmd == "DESTROY_BARRIER":
+      self.logic.destroyBarrier(thread_index)
+      print THREAD_PREFIX % (thread_index, "Barrier Destroyed")
+      rc = 0
 
-    elif cmd == "SET_TAG":
-      tag = int(args[-1])
-      pid = int(args[-2])
-      rc = self.logic.setTag(process_indexes, pid, tag)
-      print "DONE %d" % rc
+    elif cmd == "BARRIER":
+      rc = self.logic.barrier(thread_index)
+      
+      if type(rc) == list:
+        for t in rc:
+          print THREAD_PREFIX % (t, "Barrier Passed")
+        rc = 0
+      elif rc < 0:
+        print THREAD_ERROR_RC % (
+            thread_index, line_num, "Barrier Failed", rc)
 
-    elif cmd == "GET_GOOD_PROCESSES":
-      count = int(args[-1])
-      pids = self.logic.getGoodProcesses(process_indexes, count)
-      if type(pids) == int:
-        print "DONE %d" % pids
-        return pids
-      if type(pids) == list:
-        if len(pids) > 0:
-          print "DONE %d %s" % (len(pids), " ".join([str(p) for p in pids]))
-        else:
-          print "DONE 0"
-        return len(pids)
-      return -1
+    elif cmd == "SEND":
+      to = int(args[1])
+      is_sync = "SYNC" in args
+      is_urgent = "URGENT" in args
+      rc = self.logic.send(
+          thread_index, to, is_sync, is_urgent, " ".join(args[2:]))
+      
+      flags = ""
+      if is_urgent: flags += " URGENT"
+      if is_sync:   flags += " SYNC"
+      if not flags: flags = " None"
+      if rc < 0:
+        print THREAD_ERROR_RC % (
+            thread_index, line_num, "Send Failed (Flags:%s)" % flags, rc)
+      elif rc == 0:
+        print THREAD_PREFIX % (
+            thread_index, "Send Successfull (Flags:%s)" % flags)
 
-    elif cmd == "MAKE_GOOD_PROCESSES":
-      rc = self.logic.makeGoodProcesses(process_indexes)
-      print "DONE %d" % rc
-      return rc
+    elif cmd == "BROADCAST":
+      is_sync = "SYNC" in args
+      is_urgent = "URGENT" in args
+      rc = self.logic.broadcast(
+          thread_index, is_sync, is_urgent, " ".join(args[1:]))
+      
+      flags = ""
+      if is_urgent: flags += " URGENT"
+      if is_sync:   flags += " SYNC"
+      if not flags: flags = " None"
+      if rc < 0:
+        print THREAD_ERROR_RC % (
+            thread_index, line_num, "Broadcast Failed (Flags:%s)" % flags, rc)
+      elif rc == 0:
+        print THREAD_PREFIX % (
+            thread_index, "Broadcast Successfull (Flags:%s)" % flags)
 
     elif cmd == "CLOSE":
-      return self.logic.close(process_indexes)
+      self.parseLine("%d BROADCAST FINISH" % thread_index, line_num)
+      rc = self.logic.close(thread_index)
+
+      for t in range(1, self.num_threads + 1):
+        print THREAD_PREFIX % (t, "Unregistered (Thread ID = %d)" % t)
+      print "Main Thread Unregistered"
+      return OK_STOP
+
+    # After the commands, deliver all pending messages.
+    delivered = self.logic.deliverAllMessages()
+    for m in delivered:
+      # We have now received this message.
+      print THREAD_PREFIX % (
+          m.destination,
+          "Received Message (length = %d): '%s'" % (len(m.message), m.message))
+
+      # For non-sync messages, we are done.
+      if not m.is_sync:
+        continue
+
+      # If this was a broadcast, we only want to print at the end.
+      if not m.is_broadcast:
+        flags = ""
+        if m.is_urgent: flags += " URGENT"
+        if m.is_sync:   flags += " SYNC"
+        if not flags:   flags = " None"
+        print THREAD_PREFIX % (
+            m.origin, "Send Successfull (Flags:%s)" % flags)
+
+    # Finally, go over the broadcasters and print them as well.
+    broadcasters = self.logic.finishedBroadcasters()
+    for b in broadcasters:
+      flags = ""
+      if m.is_urgent: flags += " URGENT"
+      if m.is_sync:   flags += " SYNC"
+      if not flags:   flags = " None"
+      print THREAD_PREFIX % (
+          m.origin, "Broadcast Successfull (Flags:%s)" % flags)
+
+    # Return the return code from the command.
+    return rc
+    
 
 file_stream = sys.stdin
 if sys.argv[1:]:
   file_stream = file(sys.argv[1], "r")
 
-parser = TagsParser()
+parser = ThreadsParser()
 parser.parse(file_stream)
 
 if file_stream != sys.stdin:
