76bef91b71e30aeae7a41444e1f854db95103bad
igal1987 <igal1987@fdf12db7-3aba-a246-c334-511d79db4154>
2011-05-25 18:10:12 +0000

    split the tests into separates

--- trunk/OperSystems/tests/ex3/unittests/bad_send_test.c
+++ trunk/OperSystems/tests/ex3/unittests/bad_send_test.c
@@ -40,7 +40,12 @@ void write(struct test_context* con) {
   pthread_exit((void*)res);
 }
 
-test_result_t* test_send_receive_unregistered() {
+/*
+ * Case #1:
+ * The reader isn't registered.
+ * Writer tries to send a message and should fail.
+ */
+test_result_t* test_send_to_thread_that_didnt_register() {
   pthread_t writer;
   struct test_context writer_context;
   struct results* results;
@@ -49,15 +54,10 @@ test_result_t* test_send_receive_unregistered() {
   writer_context.con = mp_init();
   ASSERT_NOT_NULL(writer_context.con, "context failed to init");
   writer_context.target = pthread_self();
-  writer_context.context_null = FALSE;
-
-  /*
-   * Case #1:
-   * The reader isn't registered.
-   * Writer tries to send a message and should fail.
-   */
   writer_context.should_register = TRUE;
   writer_context.should_unregister = FALSE;
+  writer_context.context_null = FALSE;
+
   ASSERT_EQUALS_INT(0, pthread_create(&writer, NULL, (void*)&write, &writer_context),
                     "creation of writer thread failed");
 
@@ -67,12 +67,28 @@ test_result_t* test_send_receive_unregistered() {
                     "send to a thread that didn't register should fail");
   free(results);
 
-  /*
-   * Case #2:
-   * The reader is registered. the writer isn't.
-   * Writer tries to send a message and should fail.
-   */
+  /* destroy the test context */
+  mp_destroy(writer_context.con);
+  TEST_SUCCESS();
+}
+
+/*
+ * Case #2:
+ * The reader is registered. the writer isn't.
+ * Writer tries to send a message and should fail.
+ */
+test_result_t* test_send_from_thread_that_didnt_register() {
+  pthread_t writer;
+  struct test_context writer_context;
+  struct results* results;
+
+  /* build the test context */
+  writer_context.con = mp_init();
+  ASSERT_NOT_NULL(writer_context.con, "context failed to init");
+  writer_context.target = pthread_self();
   writer_context.should_register = FALSE;
+  writer_context.context_null = FALSE;
+
   mp_register(writer_context.con);
   ASSERT_EQUALS_INT(0, pthread_create(&writer, NULL, (void*)&write, &writer_context),
                     "creation of writer thread failed");
@@ -82,13 +98,33 @@ test_result_t* test_send_receive_unregistered() {
   ASSERT_EQUALS_INT(-1, results->send_result, "send to a thread that didn't register should fail");
   free(results);
 
-  /*
-   * Case #3:
-   * The reader unregistered.
-   * Writer tries to send a message and should fail.
-   */
+  /* destroy the test context */
+  mp_destroy(writer_context.con);
+  TEST_SUCCESS();
+}
+
+/*
+ * Case #3:
+ * The reader unregistered.
+ * Writer tries to send a message and should fail.
+ */
+test_result_t* test_send_to_tread_that_unregistered() {
+  pthread_t writer;
+  struct test_context writer_context;
+  struct results* results;
+
+  /* build the test context */
+  writer_context.con = mp_init();
+  ASSERT_NOT_NULL(writer_context.con, "context failed to init");
+  writer_context.target = pthread_self();
   writer_context.should_register = TRUE;
   writer_context.should_unregister = FALSE;
+  writer_context.context_null = FALSE;
+
+  mp_register(writer_context.con);
+  usleep(1000);
+  mp_unregister(writer_context.con);
+
   ASSERT_EQUALS_INT(0, pthread_create(&writer, NULL, (void*)&write, &writer_context),
                     "creation of writer thread failed");
 
@@ -98,13 +134,29 @@ test_result_t* test_send_receive_unregistered() {
                     "send to a thread that unregistered should fail");
   free(results);
   
-  /*
-   * Case #4:
-   * The reader is registered.
-   * Writer tries to send a message after unregistering, should fail.
-   */
+  /* destroy the test context */
+  mp_destroy(writer_context.con);
+  TEST_SUCCESS();
+}
+
+/*
+ * Case #4:
+ * The reader is registered.
+ * Writer tries to send a message after unregistering, should fail.
+ */
+test_result_t* test_send_from_thread_that_unregistered() {
+  pthread_t writer;
+  struct test_context writer_context;
+  struct results* results;
+
+  /* build the test context */
+  writer_context.con = mp_init();
+  ASSERT_NOT_NULL(writer_context.con, "context failed to init");
+  writer_context.target = pthread_self();
   writer_context.should_register = TRUE;
   writer_context.should_unregister = TRUE;
+  writer_context.context_null = FALSE;
+
   mp_register(writer_context.con);
   ASSERT_EQUALS_INT(0, pthread_create(&writer, NULL, (void*)&write, &writer_context),
                     "creation of writer thread failed");
@@ -114,20 +166,36 @@ test_result_t* test_send_receive_unregistered() {
   ASSERT_EQUALS_INT(-1, results->send_result, "sending from an unregistered thread should fail");
   free(results);
 
-  /*
-   * Case #5:
-   * The context is NULL.
-   * Writer tries to send a message, should fail.
-   */
+  /* destroy the test context */
+  mp_destroy(writer_context.con);
+  TEST_SUCCESS();
+}
+
+/*
+ * Case #5:
+ * The context is NULL.
+ * Writer tries to send a message, should fail.
+ */
+test_result_t* test_send_with_NULL_context() {
+  pthread_t writer;
+  struct test_context writer_context;
+  struct results* results;
+
+  /* build the test context */
+  writer_context.con = mp_init();
+  ASSERT_NOT_NULL(writer_context.con, "context failed to init");
+  writer_context.target = pthread_self();
   writer_context.should_register = TRUE;
   writer_context.should_unregister = FALSE;
   writer_context.context_null = TRUE;
+
   mp_register(writer_context.con);
   ASSERT_EQUALS_INT(0, pthread_create(&writer, NULL, (void*)&write, &writer_context),
                     "creation of writer thread failed");
   mp_unregister(writer_context.con);
 
   ASSERT_EQUALS_INT(0, pthread_join(writer, (void**)&results), "couldn't join");
+  ASSERT_EQUALS_INT(0, results->reg_result, "writer failed to register");
   ASSERT_EQUALS_INT(-1, results->send_result, "send to a NULL context should fail");
   free(results);
 
@@ -138,7 +206,11 @@ test_result_t* test_send_receive_unregistered() {
 
 test_suite_t* test_bad_send() {
   test_suite_t* results = suite_init();
-  RUN_TEST(results, test_send_receive_unregistered);
+  RUN_TEST(results, test_send_to_thread_that_didnt_register);
+  RUN_TEST(results, test_send_from_thread_that_didnt_register);
+  RUN_TEST(results, test_send_to_tread_that_unregistered);
+  RUN_TEST(results, test_send_from_thread_that_unregistered);
+  RUN_TEST(results, test_send_with_NULL_context);
 
   return results;
 }
