f617119c03ba784463caa34950ec0d4c69ded5af
igal1987 <igal1987@fdf12db7-3aba-a246-c334-511d79db4154>
2011-05-25 19:23:24 +0000

    added special target for external tests

---
+++ OperSystems/tests/ex3/unittests/external_tests.c
@@ -0,0 +1,14 @@
+#include "list_test.h"
+#include "hash_table_test.h"
+#include "context_test.h"
+#include "barrier_test.h"
+#include "bad_send_test.h"
+
+int main() {
+  test_suite_t* suite;
+  RUN_SUITE(suite, test_context);
+  RUN_SUITE(suite, test_barrier);
+  RUN_SUITE(suite, test_bad_send);
+
+  return 0;
+}
