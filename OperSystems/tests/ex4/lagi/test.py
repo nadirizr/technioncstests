379fa5ad45482481c7c8355ab969e46cf79af661
igal1987 <igal1987@fdf12db7-3aba-a246-c334-511d79db4154>
2011-06-18 16:30:21 +0000

    added option parsing

--- OperSystems/tests/ex4/lagi/test.py
+++ OperSystems/tests/ex4/lagi/test.py
@@ -1,10 +1,34 @@
 #!/usr/bin/python
 
+import sys
 import commands
 
+USAGE = "USAGE:\n\
+%s [flags]\n\
+  -NoCompile - prevents the test from compiling the code" % (sys.argv[0])
+
+def parseOptions(args):
+  should_compile = True
+  for arg in args[1:]:
+    if arg == '-NoCompile':
+      should_compile = False
+    else:
+      print USAGE
+      sys.exit(1)
+  return Options(should_compile)
+
+class Options:
+  
+  def __init__(self, should_compile=True):
+    self.should_compile = should_compile
+options = parseOptions(sys.argv)
+
+# Check whether a module already exists
 o = commands.getoutput('lsmod | grep vsf')
 if '' != o:
   print "There's a module running!"
+
+  # Try to remove the running module
   print "Trying to remove the module: ",
   o = commands.getoutput('rmmod vsf')
   if '' != o:
@@ -14,4 +38,12 @@ if '' != o:
   else:
     print "Done"
 
-print "..."
+# Now try to add the module
+# First search for the files and compile them
+print options.should_compile
+
+# Verify that adding the module with no parameter fails
+#o = commands.getoutput('
+
+
+
