38dfb2efba251e7175e4cc51e04ada9800eb5562
igal1987 <igal1987@fdf12db7-3aba-a246-c334-511d79db4154>
2011-06-18 18:20:52 +0000

    added tests

--- OperSystems/tests/ex4/lagi/test.py
+++ OperSystems/tests/ex4/lagi/test.py
@@ -32,12 +32,22 @@ def checkFilesExist(files):
   return True
 
 def test(test_name, cond):
+  if not run_test(test_name, cond):
+    sys.exit(1)
+
+def testEquals(test_name, actual, expected):
+  if not run_test(test_name, actual == expected):
+    print "  Expected: %s, Actual: %s" % (expected, actual)
+    sys.exit(1)
+    
+def run_test(test_name, cond):
   print test_name, '.' * (60 - len(test_name)),
   if not cond:
     print "FAIL"
-    sys.exit(1)
+    return False
   else:
     print "OK"
+    return True
 
 # Check whether a module already exists
 o = commands.getoutput('lsmod | grep vsf')
@@ -73,3 +83,22 @@ if not checkFilesExist(['../../vsf.o']):
 test("Test insmod without param",
      commands.getoutput('insmod ../../vsf.o').find('incorrect module parameters') != -1)
 
+# Check inserting actually works
+testEquals("Test insmod works",
+           commands.getoutput('insmod ../../vsf.o max_vsf_devices=10'),
+           '')
+# Check vsf appears in lsmod
+test("Test vsf appears in lsmod",
+     commands.getoutput('lsmod | grep vsf').startswith('vsf'))
+# Check vsf driver appears and has 10 as the max vsf
+test("Test vsf driver exists",
+     commands.getoutput('ls -l /proc/driver/vsf').startswith("-r--------"))
+driver = os.open('/proc/driver/vsf', os.O_RDONLY)
+testEquals("Check new vsf driver",
+           os.read(driver, 100),
+           '10\n')
+os.close(driver)
+# Check removing the vsf works
+testEquals("Test rmmod works",
+           commands.getoutput('rmmod vsf'),
+           '')
