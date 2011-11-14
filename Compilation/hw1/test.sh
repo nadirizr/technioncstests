ccedee524cc0c6e5a97a6c733c43f95af80fd362
igal1987 <igal1987@fdf12db7-3aba-a246-c334-511d79db4154>
2011-11-14 17:54:07 +0000

    added a few tests

--- Compilation/hw1/test.sh
+++ Compilation/hw1/test.sh
@@ -4,11 +4,14 @@
 rm -f a.out lex.yy.c
 
 # compile
-flex ../hw1.lex
-g++ lex.yy.c ../hw1.cpp
+`flex ../hw1.lex` && `g++ lex.yy.c ../hw1.cpp`
+if [ $? -ne 0 ]; then
+  echo "Compile of hw1.lex failed!"
+  exit 1
+fi
 
 # Run the tests
-tests=( "staff/t1" "lagi_tests/comment_test" "from_forum/t6" )
+tests=( "staff/t1" "lagi_tests/real_test" "lagi_tests/string_test" "lagi_tests/comment_test" "from_forum/t6" )
 
 for test in ${tests[@]}
 do
@@ -16,7 +19,7 @@ do
   ./a.out < $test.in > $test.res
   diff $test.res $test.out > $test.diff
   if [ -s $test.diff ]; then
-    echo "FAILED (see $test.diff)"
+    echo "FAILED (see tests/$test.diff)"
   else
     echo "OK"
   fi
