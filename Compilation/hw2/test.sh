e5a4d9093f3fcd2fafda9363d485865c09d0fbe5
igal1987 <igal1987@fdf12db7-3aba-a246-c334-511d79db4154>
2012-01-05 12:07:34 +0000

    added staff tests

---
+++ Compilation/hw2/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# clean up
+#rm -f a.out lex.yy.c source.tab.cpp source.tab.hpp
+
+# compile
+cd ..
+make
+#`bison -d ../hw2.ypp` && `flex ../hw2.lex` && `g++ lex.yy.c ../hw2.tab.cpp`
+if [ $? -ne 0 ]; then
+  echo "Compile of hw2.lex or hw2.ypp failed!"
+  exit 1
+fi
+
+# Run the tests
+tests=( "staff/test" )
+
+for test in ${tests[@]}
+do
+  echo -n "Running test tests/$test ..."
+  ./a.out < tests/$test.in > tests/$test.res
+  diff tests/$test.res tests/$test.out > tests/$test.diff
+  if [ -s tests/$test.diff ]; then
+    echo "FAILED (see tests/$test.diff)"
+  else
+    echo "OK"
+  fi
+done
+
