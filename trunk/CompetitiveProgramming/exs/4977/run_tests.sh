ca17ab409fcaffd1e3b3600d4ce7b36600e9de41
igal1987 <igal1987@fdf12db7-3aba-a246-c334-511d79db4154>
2012-04-03 10:01:32 +0000

    added tests

---
+++ trunk/CompetitiveProgramming/exs/4977/run_tests.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# compile
+if [ ! -f ../ex4977.cpp ]; then
+  echo "couldn't find ex4977.cpp in parent dir"
+  exit 1
+fi
+g++ ../ex4977.cpp -o ex4977
+if [ $? -ne 0 ]; then
+  echo "Compile of ex4977.cpp failed!"
+  exit 1
+fi
+
+# Run the tests
+test_dirs=( "./" )
+
+for test_dir in ${test_dirs[@]}
+do
+  for test_file in `ls ${test_dir}*.in`
+  do
+    test="${test_file:0:${#test_file}-3}"
+    echo -n "Running test tests/${test} ... "
+    ./ex4977 < $test.in > $test.res
+
+    diff $test.res $test.out > $test.diff
+    if [ -s $test.diff ]; then
+      echo "FAILED (see $test.diff)"
+    else
+      echo "OK"
+    fi
+  done
+done
